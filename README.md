d40b96d0f5e4e9aeddd34c5ff861ad92fe3c6bb9
Ashton Fei <yunjia.fei@gmail.com>
2021-03-10 18:43:18 +0800

    add job details

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# freelance-andrea-atzori
-Upwork projects for Andrea Atzori
+# Email Automation Project
+
+[Upwork](https://www.upwork.com/ab/proposals/1341293919274479617)
+
+## Google Sheets script Developer needed to email content of the sheet when a cell is updated
+
+### Job Details
+
+We are looking for a Google Sheets script developer to create a script to do the following:
+
+- when a cell is updated, for example the date in cell D9, an email is automatically sent to the recipient in cell C3
+- the content of the email has to be the cell range (B7:E25)
+- subject of the email must be pulled from cell C5
+
+Our goal is to send these emails when the report is updated. This can be every day, or once per week, so please make recommendations/suggestions in regards to the trigger if you believe that there is a better way to schedule the email.
+
+Here is the link to the test spreadsheet:
+
+https://docs.google.com/spreadsheets/d/1ubEGQlQuJaHDPtpCXXGKXPVSw8gBN_YXJVbg-OU0Ajc/edit?usp=sharing
+
+In your reply, please add "YaKKa" so that we know that you have read this brief and it's not an automated response.
+
+Any questions please ask.
---
+++ app.js
@@ -0,0 +1,133 @@
+const APP_NAME = "Email"
+const RN_RECIPIENT = "recipient" // range name of recipient
+const RN_SUBJECT = "subject" // range name of subject
+const RN_CC = "cc" // range name of cc
+const RN_BCC = "bcc" // range name of bcc
+const RN_WIDTH = "width" // range name of width
+const RN_TRIGGER = "trigger" // range name of trigger
+const RN_CONTENT = "body" // range name of email content
+
+const SN_EMAIL = "Email Template" // sheet name of email content
+
+function sendEmail() {
+  const app = new App()
+  app.sendEmail()
+}
+
+function onOpen() {
+  const ui = SpreadsheetApp.getUi()
+  const menu = ui.createMenu(APP_NAME)
+  menu.addItem("Send", "sendEmail")
+  menu.addToUi()
+}
+
+function test() {
+  new App().getEmailData()
+}
+
+class App {
+  constructor() {
+    this.ss = SpreadsheetApp.getActive()
+    this.ws = this.ss.getSheetByName(SN_EMAIL) || this.ss.getActiveSheet()
+  }
+
+  sendEmail() {
+    const trigger = this.ws.getRange(RN_TRIGGER).getValue()
+    if (!trigger) {
+      this.ss.toast(`Email trigger is ${trigger}, no email will be sent.`, APP_NAME)
+      return
+    }
+    this.ss.toast(`Sending...`, APP_NAME)
+    const { recipient, subject, options } = this.getEmailData()
+    try {
+      GmailApp.sendEmail(recipient, subject, "", options)
+      this.ss.toast(`Email has been sent to ${recipient} successfully.`, APP_NAME, 30)
+    } catch (e) {
+      this.ss.toast(e.message, APP_NAME, 30)
+    }
+  }
+
+  createHtmlBody(values, formulas, borders, width) {
+    let html = `<div style="padding: 12px;"><table style="border-collapse: collapse; width: ${width}px; margin: auto;">`
+    values.forEach((row, r) => {
+      let tr = `<tr>`
+      let style = `padding: 3px 6px;`
+      const singleValueRow = row.filter(v => v !== "")
+      const isEmptyRow = row.filter(v => v === "").length === row.length && formulas[r].filter(v => v === "").length === formulas[r].length
+      if (isEmptyRow){
+        style = `padding: 8px;`
+        tr += `<td style="${style}" colspan="${row.length}"></td>`
+      }else if (singleValueRow.length === 1) {
+        tr += `<td style="${style}" colspan="${row.length}">${singleValueRow[0]}</td>`
+      } else {
+        row.forEach((cell, c) => {
+          const border = borders[r][c]
+          const formula = formulas[r][c].toLowerCase()
+          if(formula.indexOf("=image(") !== -1) {
+            const image = formulas[r][c].split('("')[1].split('")')[0]
+            cell = `<img src="${image}" alt="image" style="width: 30px;">`
+            style += "text-align: right;"
+          }
+          if (border) {
+            if (border.top) style += "border-top: 1px solid black;"
+            if (border.right) style += "border-right: 1px solid black;"
+            if (border.bottom) style += "border-bottom: 1px solid black;"
+            if (border.left) style += "border-left: 1px solid black;"
+            tr += `<td style="${style}padding: 3px 6px;">${cell}</td>`
+          } else {
+            tr += `<td style="${style}">${cell}</td>`
+          }
+        })
+      }
+      tr += "</tr>"
+
+      html += tr
+    })
+    return html += "</table></div>"
+  }
+
+  getEmailData() {
+    const recipient = this.ws.getRange(RN_RECIPIENT).getValue()
+    const subject = this.ws.getRange(RN_SUBJECT).getValue()
+    const cc = this.ws.getRange(RN_CC).getValue()
+    const bcc = this.ws.getRange(RN_BCC).getValue()
+    const width = this.ws.getRange(RN_WIDTH).getValue()
+    const contentRange = this.ws.getRange(RN_CONTENT)
+    const contentValues = contentRange.getDisplayValues()
+    const contentFormulas = contentRange.getFormulas()
+    const contentBorders = []
+    for (let r = 0; r < contentValues.length; r++) {
+      const value = contentValues[r]
+      const contentBorder = []
+      for (let c = 0; c < value.length; c++) {
+        const border = contentRange.getCell(r + 1, c + 1).getBorder()
+        if (!border) {
+          contentBorder.push(border)
+        } else {
+          contentBorder.push({
+            top: border.getTop().getBorderStyle(),
+            right: border.getRight().getBorderStyle(),
+            bottom: border.getBottom().getBorderStyle(),
+            left: border.getLeft().getBorderStyle(),
+          })
+        }
+      }
+      contentBorders.push(contentBorder)
+    }
+
+
+    const htmlBody = this.createHtmlBody(contentValues, contentFormulas, contentBorders, width)
+    const data = {
+      recipient,
+      subject,
+      options: {
+        htmlBody,
+        cc,
+        bcc,
+      }
+    }
+    // console.log(data.options.htmlBody)
+
+    return data
+  }
+}
